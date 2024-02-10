77bae63904920008b56a728f38c34467570a4d9b
rohith <akularohith89401@gmail.com>
2024-02-10 14:34:21 +0530

    implemented endpoint

--- server.tsx
+++ server.tsx
@@ -8,16 +8,92 @@ app.use(express.json());
 
 
 // Routes
-app.post('/identities', (req, res) => {
-    dbConnection.query('select * from contacts', (err, results) => {
-        if (err) {
-            console.error('Error executing query: ' + err.stack);
-            return;
+app.post('/identities',async (req, res) => {
+    const request=req.body;
+    console.log(request);
+
+    let contactsWithSameEmail;
+    let contactsWithSamePhoneNumber;
+    
+    [contactsWithSameEmail,contactsWithSamePhoneNumber]=await Promise.all([
+        queryDatabase(`select * from contacts where email= "${request.email}" order by createdAt desc;`),
+        queryDatabase(`select * from contacts where phoneNumber= "${request.phoneNumber}" order by createdAt desc;`)
+    ]);
+
+    let emails=[];
+    let phoneNumbers=[];
+    let secondaryContactIds=[];
+    let primaryContactId;
+    emails.push(request.email);
+    phoneNumbers.push(request.phoneNumber);
+    console.log("same email");
+    contactsWithSameEmail.forEach(contact =>console.log(contact));
+    console.log("same phone number");
+    contactsWithSamePhoneNumber.forEach(contact =>console.log(contact));
+    console.log("------------------------------------------------");
+
+
+    if(contactsWithSameEmail.length==0 && contactsWithSamePhoneNumber.length==0){
+        let response=await queryDatabase(`INSERT INTO contacts ( phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt, deletedAt) 
+        VALUES ( '${request.phoneNumber}', '${request.email}', null, 'primary', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', null);`)
+        primaryContactId=response.insertId;
+    }else if(contactsWithSameEmail.length>0 && contactsWithSamePhoneNumber.length>0){
+        if(contactsWithSameEmail[0].createdAt<contactsWithSamePhoneNumber[0].createdAt){
+            primaryContactId=contactsWithSameEmail[0].id;
+            contactsWithSamePhoneNumber[0].linkPrecedence='secondary';
+        }else{
+            primaryContactId=contactsWithSamePhoneNumber[0].id;
+            contactsWithSameEmail[0].linkPrecedence='secondary';
         }
-        console.log('Query results:', results);
-    });
-    res.status(201).send('successfull request');
+        var allContacts=[...contactsWithSamePhoneNumber,...contactsWithSameEmail]
+        allContacts.forEach(contact=>{
+            emails.push(contact.email);
+            phoneNumbers.push(contact.phoneNumber);
+            if(contact.linkPrecedence=='secondary'){
+                secondaryContactIds.push(contact.id);
+            }
+        })
+    }else{
+        
+        var allContacts=[...contactsWithSamePhoneNumber,...contactsWithSameEmail]
+        allContacts.forEach(contact=>{
+            
+            emails.push(contact.email);
+            phoneNumbers.push(contact.phoneNumber);
+            if(contact.linkPrecedence=='secondary'){
+                secondaryContactIds.push(contact.id);
+            }
+            if(contact.linkPrecedence=='primary'){
+                primaryContactId=contact.id;
+            }
+        })
+        let response=await queryDatabase(`INSERT INTO contacts ( phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt, deletedAt) 
+        VALUES ( '${request.phoneNumber}', '${request.email}', ${primaryContactId}, 'secondary', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', null);`)
+        secondaryContactIds.push(response.insertId);
+    }
+
+    res.status(201).send({"contact":{
+        "primaryContatctId":primaryContactId,
+        "emails": [...new Set(emails)],
+        "phoneNumbers": [...new Set(phoneNumbers)],
+        "secondaryContactIds": [...new Set(secondaryContactIds)]
+    }});
 });
+
+// Helper function to execute a query
+function queryDatabase(sql) {
+    return new Promise((resolve, reject) => {
+        dbConnection.query(sql, (error, results) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+            } else {
+                // console.log(results);
+                resolve(results);
+            }
+        });
+    });
+}
 
 // Start server
 app.listen(PORT, () => {
